e54a6ca0bf32ee12bc7c857586a69401e8c2d31d
cerradamartos <cerradamartos@gmail.com>
2018-03-27 22:18:24 +0200

    Add files via upload
    Created a scheme for the system and corrected the code.

--- riegoAutomatico.ino
+++ riegoAutomatico.ino
@@ -1,25 +1,32 @@
-int i;
 int hum = analogRead(A0);
 int humPercentage;
+
 void setup() {
   Serial.begin(9600);
   Serial.println ("Iniciando el riego automatico...");
   delay (5000);
   pinMode (LED_BUILTIN, OUTPUT);
+  pinMode (6, OUTPUT);//Bomaba de agua
+  pinMode (9, OUTPUT);//Zumbador
+  pinMode (A0, INPUT);//Sensor de humedad
 }
 
 void loop() {
   humPercentage = map(hum, 0, 1023, 0, 100);
   Serial.println (humPercentage);
   delay(100);
+
+
   //Riego
   if (humPercentage > 0 && humPercentage < 70) {
     Serial.println ("Regando las plantas");
     delay(100);
     digitalWrite (LED_BUILTIN, HIGH);
     digitalWrite (6, HIGH);//El pin de la bomba de agua se enciende
+    digitalWrite (9, LOW);
+
   }
-  //ERROR 1: Sensor mal colocado (fuera de la maceta, fallo de la bomba, etc.)
+  //ERROR 1: Sensor mal colocado
   else if (humPercentage == 0) {
     Serial.println ("ERROR 1: Sensor mal colocado");
     delay(100);
@@ -28,9 +35,23 @@ void loop() {
     digitalWrite (LED_BUILTIN, LOW);
     delay(1000);
     digitalWrite (6, LOW);//El pin de la bomba de agua queda apagado
+    digitalWrite (9, HIGH);//El zumbador se enciende
+
+  }
+  //ERROR 2: Fallo en la bomba
+  else if (humPercentage >= 97) {
+    Serial.println ("ERROR 2: Fallo en la bomba");
+    delay(100);
+    digitalWrite (LED_BUILTIN, HIGH);
+    delay(100);
+    digitalWrite (LED_BUILTIN, LOW);
+    delay(100);
+    digitalWrite (6, LOW);//El pin de la bomba de agua queda apagado
+    digitalWrite (9, HIGH);//El zumbador se enciende
+
   }
   //Plantas regadas
-  else if (humPercentage >= 70) {
+  else if (humPercentage >= 70 || humPercentage < 95) {
     Serial.println ("Plantas regadas");
     delay(100);
     digitalWrite (LED_BUILTIN, HIGH);
@@ -38,9 +59,9 @@ void loop() {
     digitalWrite (LED_BUILTIN, LOW);
     delay(3000);
     digitalWrite (6, LOW);//El pin de la bomba de agua queda apagado
+    digitalWrite (9, LOW);
+
+
   }
-
-
-
-
 }
+
